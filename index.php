810c344110470fa63180a6d863f2e039bc994073
stevenasher <36062665+stevenasher@users.noreply.github.com>
2018-04-13 16:20:28 +0800

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 	<link rel="icon" href="icon.png" sizes="16x16">
     <title>My Connection</title>
 </head>
-<frameset rows="130,*,70" frameborder="0" border="0" framespacing="0">
+<frameset rows="150,*,70" frameborder="0" border="0" framespacing="0">
     <frame name="topNav" src="top_nav.php">
     <frameset cols="230,*" frameborder="0" border="0" framespacing="0">
         <frame name="menu" src="menu_1.php" marginheight="0" marginwidth="0" scrolling="auto" noresize>
